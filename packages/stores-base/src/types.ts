1d771da2ab1717fe8af6ebb177617c6c5ea50b5e
WHenderson <github@whenderson.maily.dev>
2022-03-20 12:55:56 +1100

    Adding documentation

--- packages/stores-base/src/types.ts
+++ packages/stores-base/src/types.ts
@@ -1,8 +1,8 @@
 /** Callback to inform of a value updates. */
 export type Subscriber<T> = (value: T) => void;
 
-/** Generic action. */
-export type Action = () => void;
+import { Action } from '@crikey/stores-base-queue';
+export { Action } from '@crikey/stores-base-queue';
 
 /** Unsubscribes from value updates. */
 export type Unsubscriber = Action;
@@ -25,30 +25,46 @@ export type StartNotifier<T> = (set: Subscriber<T>, invalidate: Invalidator, rev
 /** Callback used to determine if a change signal should be emitted */
 export type Trigger<T> = (initial: boolean, new_value: T, old_value?: T) => boolean;
 
+/** Basic subscription signature used by terminal subscriptions */
 export type SubscribeBasic<T> = (this: void, run: Subscriber<T>) => Unsubscriber;
+
+/** Full subscription signature used derived store types */
 export type SubscribeFull<T> = (this: void, run: Subscriber<T>, invalidate?: Invalidator, revalidate?: Revalidator) => Unsubscriber;
+
+/** Subscription signature */
 export type Subscribe<T> = SubscribeBasic<T> | SubscribeFull<T>;
 
-/** Readable interface for subscribing. */
+/**
+ * Readable interface for subscribing.
+ *
+ * @category Core
+ */
 export interface Readable<T> {
     /**
      * Subscribe on value changes.
+     *
      * @param run subscription callback - calls are queued
      * @param invalidate cleanup callback - calls are immediate
      */
     subscribe: Subscribe<T>;
 }
 
-/** Writable interface for both updating and subscribing. */
+/**
+ * Writable interface for both updating and subscribing.
+ *
+ * @category Core
+ */
 export interface Writable<T> extends Readable<T> {
     /**
      * Set value and inform subscribers.
+     *
      * @param value to set
      */
     set(this: void, value: T): void;
 
     /**
      * Update value using callback and inform subscribers.
+     *
      * @param updater callback
      */
     update(this: void, updater: Updater<T>): void;
