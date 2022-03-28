f8255f85463c7edd553279439483496638c6f920
WHenderson <github@whenderson.maily.dev>
2022-03-28 21:58:41 +1100

    documentation

--- packages/stores-base/src/constant.ts
+++ packages/stores-base/src/constant.ts
@@ -4,6 +4,7 @@ import {noop} from "./noop";
 /**
  * Create a simple store which always returns the same value upon subscription
  *
+ * @category Create Store
  * @param value the constant value of the store
  */
 export function constant<T>(value: T): Readable<T> {
