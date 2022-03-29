cdeacf724d3b3c4f46aa3029d49e0eca6e48c552
WHenderson <github@whenderson.maily.dev>
2022-03-29 12:39:00 +1100

    Add typescript validation scripts

--- packages/stores-immer/test/index.test.ts
+++ packages/stores-immer/test/index.test.ts
@@ -4,5 +4,4 @@ import * as strict from "@crikey/stores-strict"
 
 it('should be based on strict types', () => {
     expect(immer.readable).toBe(strict.readable);
-    expect(immer.trigger_strict_not_equal).toBe(strict.trigger_strict_not_equal);
 })
