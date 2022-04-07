5d2a80abeaf3529bbec8569dc7f3b7f461bc1cb2
WHenderson <github@whenderson.maily.dev>
2022-04-07 21:07:24 +1000

    more lenient throttle timings in example

--- packages/stores-temporal/examples/throttle.test.ts
+++ packages/stores-temporal/examples/throttle.test.ts
@@ -9,55 +9,43 @@ it('example-throttle', async () => {
 
     // #region example-throttle
 
+    const wait = (ms: number) => new Promise(resolve => {
+        setTimeout(resolve, ms);
+    })
+
     const store = writable(trigger_strict_not_equal, 1);
 
-    const throttled = throttle(store, 100);
+    const throttled = throttle(store, 500);
 
     throttled.subscribe(value => console.log(value));
 
-    await new Promise(resolve => {
-        setTimeout(resolve, 10);
-    });
+    await wait(10);
     store.set(2);
-    await new Promise(resolve => {
-        setTimeout(resolve, 10);
-    });
+    await wait(10);
     store.set(3);
-    await new Promise(resolve => {
-        setTimeout(resolve, 10);
-    });
+    await wait(10);
     store.set(4);
 
-    await new Promise(resolve => {
-        setTimeout(resolve, 150);
-    });
+    await wait(500);
     store.set(5);
-    await new Promise(resolve => {
-        setTimeout(resolve, 10);
-    });
+    await wait(200);
     store.set(6);
-    await new Promise(resolve => {
-        setTimeout(resolve, 10);
-    });
+    await wait(200);
     store.set(7);
-    await new Promise(resolve => {
-        setTimeout(resolve, 10);
-    });
+    await wait(200);
     store.set(8);
-    await new Promise(resolve => {
-        setTimeout(resolve, 150);
-    });
+    await wait(600);
 
     // > 1
     // > 4
-    // > 5
+    // > 7
     // > 8
     // #endregion example-throttle
 
     expect(console.log.mock.calls).to.deep.equal([
         [1],
         [4],
-        [5],
+        [7],
         [8],
     ]);
 });
