f2b3d404085bff3376d10aa13840379be1ee596b
WHenderson <github@whenderson.maily.dev>
2022-04-22 23:46:02 +1000

    add tests for unhandled subscriber exceptions

--- packages/stores-base/test/writable.test.ts
+++ packages/stores-base/test/writable.test.ts
@@ -1,5 +1,11 @@
-import { expect, it } from 'vitest'
+import {expect, it, vi} from 'vitest'
 import {get, trigger_always, writable} from "../src";
+import {
+    get_store_runner,
+    set_store_runner,
+    store_runner_throw_errors,
+    StoreRunner
+} from "@crikey/stores-base-queue/src";
 
 it('should update with each change', () => {
     const store = writable(trigger_always, 1);
@@ -16,4 +22,42 @@ it('should update with each change', () => {
 
     expect(get(store)).toBe(2);
     expect(count).toBe(3);
-})
+});
+
+const run = (runner: StoreRunner, action: () => void) => {
+    const originalRunner = set_store_runner(runner);
+    expect(get_store_runner()).to.equal(runner);
+    try {
+        action();
+    } finally {
+        expect(get_store_runner()).to.equal(runner);
+        set_store_runner(originalRunner);
+        expect(get_store_runner()).to.equal(originalRunner);
+    }
+}
+
+it('should perform cleanup even during an unhandled exception', () => {
+    run(store_runner_throw_errors, () => {
+        const started = vi.fn();
+        const stopped = vi.fn();
+        const store = writable(trigger_always, 1, () => {
+            started();
+            return stopped;
+        });
+
+        expect(() => {
+            store.subscribe(
+                _value => {
+                    throw Error('unhandled exception');
+                }
+            );
+        }).toThrow('unhandled exception');
+
+        expect(started.mock.calls.length).toBe(1);
+        expect(stopped.mock.calls.length).toBe(1);
+
+        expect(() => {
+            store.set(2);
+        }).not.toThrow();
+    });
+});
