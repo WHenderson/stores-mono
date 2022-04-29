6e7001d25baee7ef0c375a982cca108406b28417
WHenderson <github@whenderson.maily.dev>
2022-04-29 18:21:41 +1000

    correct example meta data

--- packages/stores-dynamic/examples/constant.test.ts
+++ packages/stores-dynamic/examples/constant.test.ts
@@ -22,13 +22,13 @@ it('example-constant', () => {
 it('example-constant-value', () => {
     const console = shim_console();
 
-    // #region example-constant
+    // #region example-constant-value
     const store = constant_value(10);
 
     console.log('store value:', get(store));
 
     // > store value: undefined
-    // #endregion example-constant
+    // #endregion example-constant-value
 
     expect(console.log.mock.calls).to.deep.equal([
         ['store value:', { value: 10, is_const: true }],
@@ -38,14 +38,14 @@ it('example-constant-value', () => {
 it('example-constant-error', () => {
     const console = shim_console();
 
-    // #region example-constant
+    // #region example-constant-error
     const error = new Error('my error');
     const store = constant_error(error);
 
     console.log('store value:', get(store));
 
     // > store value: undefined
-    // #endregion example-constant
+    // #endregion example-constant-error
 
     expect(console.log.mock.calls).to.deep.equal([
         ['store value:', { error, is_const: true }],
