22a348e89aa3ca40346c02f14a3a9f6e6a9eaf6a
WHenderson <github@whenderson.maily.dev>
2022-07-17 12:48:19 +1000

    * fix: incorrect import

--- packages/stores-selectable/src/is_selectable.ts
+++ packages/stores-selectable/src/is_selectable.ts
@@ -1,6 +1,6 @@
 import {Readable} from "@crikey/stores-base";
 import {Selectable} from "./types";
-import {Writable} from "@crikey/stores-base/src";
+import {Writable} from "@crikey/stores-base";
 
 export function is_selectable<T = unknown, P = unknown>(writable: Writable<T>): writable is Selectable<T, Writable<T>, P>;
 export function is_selectable<T = unknown, P = unknown>(readable: Readable<T>): readable is Selectable<T, Readable<T>, P>;
