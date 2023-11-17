c7aefd923b48e0a1fd113428db4c0f93a310cc61
WHenderson <tower@direct.maily.dev>
2023-11-17 11:47:08 +1100

    by_set tests and bug fixes

---
+++ packages/stores-select/test/by_set.test.ts
@@ -0,0 +1,21 @@
+import {expect, it} from "vitest";
+import {by_set} from "../src";
+
+it('should access child properties', function () {
+    const original = new Set(['a']);
+
+    const selector_a = by_set<string>('a');
+
+    expect(selector_a.get(original)).toBe(true);
+    expect(selector_a.update(original, false)).to.deep.equal(new Set([]));
+    expect(selector_a.update(original, true)).toBe(original);
+
+    const selector_b = by_set<string>('b');
+
+    expect(selector_b.get(original)).toBe(false);
+    expect(selector_b.update(original, true)).to.deep.equal(new Set(['a','b']));
+    expect(selector_b.delete(selector_b.update(original, true))).to.deep.equal(new Set(['a']));
+    expect(selector_b.delete(original)).toBe(original);
+
+    expect(original, 'original should not be mutated').to.deep.equal(new Set(['a']));
+});
