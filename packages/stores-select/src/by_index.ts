04b42e61227d7aaeaaf73a76b62a8078451df7fc
WHenderson <tower@direct.maily.dev>
2023-11-17 12:02:50 +1100

    by_index adding tests and allowing sparse delete

--- packages/stores-select/src/by_index.ts
+++ packages/stores-select/src/by_index.ts
@@ -1,9 +1,20 @@
-import {ReadSelector, WriteSelector} from "./types";
+import {DeleteSelector, ReadSelector, WriteSelector} from "./types";
+
+
+export function by_index<T extends Array<E | undefined>,I extends keyof T & number, E>(
+    index: I,
+    def?: () => E | undefined
+) : ReadSelector<T, E | undefined> & WriteSelector<T, E | undefined> & DeleteSelector<T>;
 
 export function by_index<T extends Array<E>,I extends keyof T & number, E>(
     index: I,
     def?: () => E
-) : ReadSelector<T, E> & WriteSelector<T, E> {
+) : ReadSelector<T, E> & WriteSelector<T, E>;
+
+export function by_index<T extends Array<E>,I extends keyof T & number, E>(
+    index: I,
+    def?: () => E
+) : ReadSelector<T, E> & WriteSelector<T, E> & DeleteSelector<T> {
     const default_ = def ?? (() => { throw new Error('index not found') });
 
     return {
@@ -19,6 +30,14 @@ export function by_index<T extends Array<E>,I extends keyof T & number, E>(
 
             const updated = [...parent];
             updated[index] = value;
+            return <T>updated;
+        },
+        delete(parent) {
+            if (!Object.hasOwn(parent, index))
+                return parent;
+
+            const updated = [...parent];
+            delete updated[index];
             return <T>updated;
         }
     }
