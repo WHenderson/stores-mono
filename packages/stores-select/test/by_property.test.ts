ea504ff38216bd0407e352f63f5334481c33cf16
WHenderson <tower@direct.maily.dev>
2023-11-17 11:32:45 +1100

    adding default check

--- packages/stores-select/test/by_property.test.ts
+++ packages/stores-select/test/by_property.test.ts
@@ -22,5 +22,8 @@ it('should access child properties', function () {
     expect(selector_b.delete(selector_b.update(original, 2))).to.deep.equal({ a: 1 });
     expect(selector_b.delete(original)).toBe(original);
 
+    const selector_b_def = by_property<Obj, keyof Obj>('b', () => 42);
+    expect(selector_b_def.get(original)).toBe(42);
+
     expect(original, 'original should not be mutated').to.deep.equal({ a: 1 });
 });
