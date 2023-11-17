cf831d30cce2aa0e7a3351f55a433e03e6b00db7
WHenderson <tower@direct.maily.dev>
2023-11-17 13:51:58 +1100

    by_index shouldnt allow delete (use by_sparse_index for that)

--- packages/stores-select/src/by_index.ts
+++ packages/stores-select/src/by_index.ts
@@ -1,15 +1,15 @@
-import {DeleteSelector, ReadSelector, WriteSelector} from "./types";
+import {ReadSelector, WriteSelector} from "./types";
 
 
 export function by_index<T extends Array<E>,I extends keyof T & number, E>(
     index: I,
     def?: () => T[0]
-) : ReadSelector<T, T[0]> & WriteSelector<T, T[0]> & DeleteSelector<T>;
+) : ReadSelector<T, T[0]> & WriteSelector<T, T[0]>;
 
 export function by_index<T extends Array<E>,I extends keyof T & number, E>(
     index: I,
     def?: () => E
-) : ReadSelector<T, E> & WriteSelector<T, E> & DeleteSelector<T> {
+) : ReadSelector<T, E> & WriteSelector<T, E> {
     const default_ = def ?? (() => { throw new Error('index not found') });
 
     return {
@@ -25,14 +25,6 @@ export function by_index<T extends Array<E>,I extends keyof T & number, E>(
 
             const updated = parent.slice();
             updated[index] = value;
-            return <T>updated;
-        },
-        delete(parent) {
-            if (!Object.hasOwn(parent, index))
-                return parent;
-
-            const updated = parent.slice();
-            delete updated[index];
             return <T>updated;
         }
     }
